597a3723f79be84028405944da9bd6433098108b
karakib2k18 <karakib2k18@gmail.com>
2021-10-04 23:37:33 +0600

    spinner-add

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,15 +4,26 @@ import "./Blog.css"
 
 const Blog = () => {
     const [blogposts, setBlogposts] = useState([]);
+    const [isShowSpinner, setIsShowSpinner] = useState(true)
     useEffect(() => {
         fetch('./blog.JSON')
             .then(response => response.json())
-            .then(json => setBlogposts(json))
+            .then(json => {
+                setBlogposts(json);
+                setIsShowSpinner(false);
+            })
     }, []);
     return (
         <div className="container mt-4">
             <h1 className="mb-4">Our Blogs</h1>
             <Row xs={1} md={3} className="g-4">
+                {isShowSpinner && (
+                    <div class="d-flex justify-content-center align-items-center">
+                        <div class="spinner-border  " role="status">
+                            <span class="visually-hidden ">Loading...</span>
+                        </div>
+                    </div>
+                )}
                 {blogposts.map(blog => (
                     <Col>
                         <Card className="shadow p-3 bg-white rounded h-100 text-start blogedit">
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,16 +7,27 @@ import CardDetails from '../CardDetails/CardDetails';
 
 const Home = () => {
     const [courses, setCourses] = useState([]);
+    const [isShowSpinner, setIsShowSpinner] = useState(true)
     useEffect(() => {
         fetch('./course.JSON')
             .then(response => response.json())
-            .then(json => setCourses(json))
+            .then(json => {
+                setCourses(json);
+                setIsShowSpinner(false);
+            })
     }, []);
     return (
         <div>
             <h1 className="mt-3 mb-3">Popular Courses</h1>
             <div className="container">
                 <Row xs={1} md={3} className="g-4">
+                    {isShowSpinner && (
+                        <div class="d-flex justify-content-center align-items-center">
+                            <div class="spinner-border  " role="status">
+                                <span class="visually-hidden ">Loading...</span>
+                            </div>
+                        </div>
+                    )}
                     {courses.slice(0, 4).map(course => (
                         <CardDetails key={course.id} course={course}></CardDetails>
                     ))}
